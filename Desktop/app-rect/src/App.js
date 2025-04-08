20a1c5d073348532697d6800bb9f5910094e026d
yamnaett <yamnaettahery2@gmail.com>
2025-04-08 11:35:12 +0100

    Ajout du formulaire d'inscription

---
+++ Desktop/app-rect/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import SignupForm from './SignupForm';
+
+function App() {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
